8e7b69512dcb93fc3046f70244dc3c61d85976d4
Stanislav <stas19882004@gmail.com>
2015-09-11 16:43:59 -0400

    Fix bug

--- app/code/local/Stasevi4/Autoupsell/Model/Collection.php
+++ app/code/local/Stasevi4/Autoupsell/Model/Collection.php
@@ -21,7 +21,7 @@ class Stasevi4_Autoupsell_Model_Collection extends Mage_Core_Model_Abstract {
 				}
 				$cats_collection = Mage::getModel('catalog/category')->getCollection()
 					->addAttributeToFilter('entity_id', $categoryIds)
-					->addFieldToFilter('store_id', Mage::app()->getStore()->getId())
+					->setStoreId( Mage::app()->getStore()->getId())
 					->addAttributeToFilter('is_active', 1);
 				$cats_collection->getSelect()->order($level )->limit(1);
 				$cats_collection->load();
@@ -92,23 +92,24 @@ class Stasevi4_Autoupsell_Model_Collection extends Mage_Core_Model_Abstract {
 
 				$cats_collection = Mage::getModel('catalog/category')->getCollection()
 				->addAttributeToFilter('entity_id', $categoryIds)
-				->addFieldToFilter('store_id', Mage::app()->getStore()->getId())
+				->setStoreId( Mage::app()->getStore()->getId())
 				->addAttributeToFilter('is_active', 1);
 
 				$cats_collection->getSelect()->order('level DESC')->limit(1);
 				$cats_collection->load();
 
+
 				foreach($cats_collection as $category){
 
 				}
-				
 
 			}
-	
+
 			if (isset($category)) {
 				if ($limit === false) {
 					$limit = Mage::getStoreConfig('autoupsell/generalrelated/limit');
 				}
+
 				$products = Mage::getResourceModel('reports/product_collection')
 					->addAttributeToFilter('visibility', array(
 						Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH,
@@ -120,17 +121,16 @@ class Stasevi4_Autoupsell_Model_Collection extends Mage_Core_Model_Abstract {
 					->addStoreFilter()
 					->setPageSize($limit);
 
-		
 				if( Mage::getSingleton('core/session')->getUpsellProductIds()) {
 					$products->addAttributeToFilter('entity_id', array(
 						'nin' => array_values(Mage::getSingleton('core/session')->getUpsellProductIds()))
 					);
 					
-				}	
-					
+				}
+
 				if ($product) {
 					$products->addAttributeToFilter('entity_id', array(
-						'neq' => Mage::registry('current_product')->getId())
+						'neq' => $product->getId())
 					);
 				}
 
